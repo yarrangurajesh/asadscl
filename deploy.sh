fb69e19734f918278d89f500efd00e7a0e63cbec
NaveenkumarNK3 <navin666n@gmail.com>
2022-10-29 19:27:54 +0530

    ok

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,1 @@
-updated in stage to demonstrate the pr
-<<<<<<< HEAD
-hey there
-i am learning a new time chaning the master programme
-by using the command
-git branch
-and git checkout name
-=======
-hey there 
-i am learing the git branch of the git bash
-here you can change the master as well
-intresting know 
-by using commmand git checkout name
->>>>>>> development
+updating the file from my local repo
